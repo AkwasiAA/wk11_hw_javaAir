8191ac919f6775e0bb982d79984738363de50447
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-08-28 20:24:34 +0100

    6th commit planetype enum complete

--- src/main/java/Airport/Plane/PlaneType.java
+++ src/main/java/Airport/Plane/PlaneType.java
@@ -1,4 +1,25 @@
 package Airport.Plane;
 
 public enum PlaneType {
+
+    BOEING777(368, 134800),
+    AIRBUSA380(555, 277000),
+    EMBRAER190(98, 27750),
+    BOMBARDIERGLOBAL7500(19, 27987);
+
+    private final int maxCapacity;
+    private final double totalWeight;
+
+    PlaneType(int maxCapacity, double totalWeight){
+        this.maxCapacity = maxCapacity;
+        this.totalWeight = totalWeight;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public double getTotalWeight() {
+        return totalWeight;
+    }
 }
