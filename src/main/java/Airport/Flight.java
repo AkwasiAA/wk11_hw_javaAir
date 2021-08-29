a4c0628383cbd8aa90824eed4e1dbf7e2bff9f2f
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-08-29 14:48:13 +0100

    11th commit started flight class

--- src/main/java/Airport/Flight.java
+++ src/main/java/Airport/Flight.java
@@ -1,4 +1,31 @@
 package Airport;
 
+import Airport.Plane.Plane;
+import CabinCrew.CabinCrewMember;
+import CabinCrew.Pilot;
+
+import java.util.ArrayList;
+
 public class Flight {
+
+    private Pilot pilot;
+    private ArrayList<CabinCrewMember> flightCrew;
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destination;
+    private String departureAirport;
+    private String departureTime;
+
+    public Flight(Pilot pilot, Plane plane, String flightNumber, String destination, String departureAirport, String departureTime){
+        this.pilot = pilot;
+        this.flightCrew = new ArrayList<>();
+        this.passengers = new ArrayList<>();
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
 }
